0f70496c0977319f78df3fc5bf7640a63f10615f
Yashwant <gandhamyashwant123@gmail.com>
2026-05-15 21:21:21 -0600

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        mDic = {}
+
+        for i in nums:
+            mDic[i] = mDic.get(i,0)+1
+
+        # return max(mDic, key=mDic.get)
+        return max(mDic, key=lambda k: mDic[k])
